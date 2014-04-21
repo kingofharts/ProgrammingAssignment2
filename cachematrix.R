98c5ec562e1f099b6ded191cb7e220ca4033cfbd
nicholas.paul.hartman <nicholas.paul.hartman@gmail.com>
2014-04-21 14:45:59 -0700

    2nd commit attempt at 2nd Programming Assignment, with additional comment write up

--- cachematrix.R
+++ cachematrix.R
@@ -1,38 +1,41 @@
 ## Reply to Peer Assessed 2nd Programming Assignment for R Programming
 ## nicholas.paul.hartman
 
-## Create list of functions to set, get, invert, and retrieve inversion of
-## a Matrix
+## Create list of functions to set, get, invert, and retrieve the 
+## inversion of a Matrix
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(x = matrix()) {     # function name & arguments
         
-        m <- NULL
-        set <- function(y) {
-                x <<- y
-                m <<- NULL
+        m <- NULL                               # create cache object
+        set <- function(y) {                    # create 'set', list.func 1
+                x <<- y                         # assign new values to x
+                m <<- NULL                      # reset cache object
         }
-        get <- function() x
+        get <- function() x                     # create 'get', list.func 2
         setinverse <- function(solve) m <<- solve
+                # create 'setinverse', list.func 3; eval, assign cache obj
         getinverse <- function() m
+                # create 'getinverse', list.func 4; retrieve cache obj
         list(set = set, get = get,
              setinverse = setinverse,
-             getinverse = getinverse)
+             getinverse = getinverse)   # compile func list
         
 }
 
 
-## Return a matrix that is the inverse of 'x'
+## Return cached - or cache if uncached and then return - 
+## inversion of a matrix
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(x, ...) {                # function name & arguments
         
-        m <- x$getinverse()
-        if(!is.null(m)) {
-                message("getting cached inverse")
-                return(m)
-        }
-        data <- x$get()
-        m <- solve(data, ...)
-        x$setinverse(m)
-        m
+        m <- x$getinverse()             # create, assign func returned obj
+        if(!is.null(m)) {               # if rtrnd obj pre-calc'ed,
+                message("getting cached inverse")       # indicate and
+                return(m)                               # return obj
+        }                                               # else
+        data <- x$get()         # retrieve values (matrix) for evaluation
+        m <- solve(data, ...)   # invert values (matrix), assign rtrnd obj
+        x$setinverse(m)         # assign cache obj for future retrieval
+        m                       # return matrix inversion
         
 }
